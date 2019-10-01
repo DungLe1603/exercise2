fb77a3aaf9055be7d9d3554b662d439195d219a9
unknown <dungle160396@gmail.com>
2019-10-01 12:08:31 +0700

    [exercise_2.1] show list and detail

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -4,19 +4,52 @@ require_once("Model/News.php");
 
 class  NewsController
 {
+	private $modelNews;
+
+	public function __construct() {
+		$this->modelNews = new News();
+	}
+
+	public function handle()
+	{
+		$action = isset($_GET['ac']) ? $_GET['ac'] : '';
+		$id = isset($_GET['id']) ? $_GET['id'] : '';
+		switch ($action) {
+			case '':
+				$this->index();
+				break;
+			case 'add':
+				$this->create();
+				break;
+			case 'show':
+				$this->show($id);
+				break;
+			case 'edit':
+				$this->edit($id);
+				break;
+			case 'delete':
+				$this->delete($id);
+				break;
+			default:
+				echo "Trang không tồn tại!";
+				break;
+		}
+	}
+
 	public function index()
 	{
-		$modelNews = new News();
-		if (!isset($_GET['id'])) {
-			$list = $modelNews->getAllNews();
-			include_once("View/list.php");
-		} else {
-			$detail = $modelNews->getNewsDetail($_GET['id']);
+		$list = $this->modelNews->getAllNews();
+		include_once("View/list.php");
+	}
+
+	public function show($id)
+	{
+		if ($id) {
+			$detail = $this->modelNews->getNewsDetail($id);
 			include_once("View/show.php");
+		} else {
+			echo "Trang không tồn tại!";
 		}
 
 	}
 };
-
-$news = new NewsController();
-$news->index();
